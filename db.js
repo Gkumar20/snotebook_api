cb9c37adf86bad7c22f779157597c897e5f9a790
Ganesh Kumar <ganeshkr6754@gmail.com>
2023-04-09 22:08:42 +0530

    conection

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
  const mongoose = require('mongoose');
- const mongoURI = "mongodb://0.0.0.0:27017/snotebook" //connected at this localhost
+//  const mongoURI = "mongodb://0.0.0.0:27017/snotebook" //connected at this localhost
+ const mongoURI =  'mongodb+srv://Gkumar20:Guddoo7322@cluster0.gun7wr9.mongodb.net/?retryWrites=true&w=majority'
 
 
  //there is build connectiona and checking connection error and return promise as then and catch
  const connectToMongo = ()=>{
-    mongoose.connect(mongoURI)
+    mongoose.connect(mongoURI,
+      { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {     
       console.log("Successfully connected");
     })
